dc0c3f8b78a83a5619f67af086a1e945850870eb
naoketa <49198895+naoketa@users.noreply.github.com>
2019-08-08 06:12:27 -0400

    add: 限度額を超えたら使えない

--- src/main/java/com/example/CreditCard.java
+++ src/main/java/com/example/CreditCard.java
@@ -5,11 +5,24 @@ import java.util.Objects;
 
 public class CreditCard {
   private BigDecimal limit;
+  private BigDecimal used = BigDecimal.ZERO;
 
   public void withdraw(int amount) {
     if(limitNotAssigned()){
       throw new IllegalStateException();
     }
+    if(notEnoughMoney(amount)){
+      throw new IllegalStateException();
+    }
+  }
+
+  public void setLimit(BigDecimal limit){
+    this.limit = limit;
+  }
+
+  private boolean notEnoughMoney(int amount) {
+    BigDecimal available = limit.subtract(used);
+    return available.compareTo(BigDecimal.valueOf(amount)) < 0;
   }
 
   private boolean limitNotAssigned(){
--- src/test/java/com/example/CreditCardTest.java
+++ src/test/java/com/example/CreditCardTest.java
@@ -2,6 +2,7 @@ package com.example;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 
 public class CreditCardTest {
@@ -12,4 +13,11 @@ public class CreditCardTest {
     assertThrows(IllegalStateException.class, () -> creditCard.withdraw(1));
   }
 
+  @Test
+  public void cannot_withdraw_when_not_enough_money() {
+    CreditCard creditCard = new CreditCard();
+    creditCard.setLimit(BigDecimal.ONE);
+    assertThrows(IllegalStateException.class, () -> creditCard.withdraw(2));
+  }
+  
 }
